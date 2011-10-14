4159fe875e0715f3c35bb4a4779eee9724b0c90b
Duane R. Bailey <bailey.d.r@gmail.com>
2011-10-14 16:01:56 -0400

    Made small changes to build.sh moving the resulting Test.app from root directory to build directory.

--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ ${CC} $INCLUDE $OPT c/Source/sketch.c -c -o build/sketch.o || exit 1
 ${CC} $INCLUDE $OPT test.c -c -o build/test.o || exit 1
 ${CC} build/EventLoop.o build/SketchApplicationDelegate.o build/SketchView.o build/SoftwareDrawable.o build/sketch.o build/test.o -o build/test -framework Cocoa || exit 1
 tools/darwin/bundle.sh Test test build/test test || exit 1
+rm -r build/Test.app;
+mv Test.app/ build/Test.app/ || exit 1
