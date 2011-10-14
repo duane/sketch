d55c5bec926f2289430f3b394e004b2c69dc61ac
Duane R. Bailey <bailey.d.r@gmail.com>
2011-10-14 15:46:48 -0400

    Cleaned up application code. No menu code is left, but the code removed was not functional.

--- c/Source/Darwin/SketchApplicationDelegate.m
+++ c/Source/Darwin/SketchApplicationDelegate.m
@@ -9,40 +9,18 @@
   return self;
 }
 
-- (void)createMenu: (NSString *)appName {
-  NSMenu *appMenu = [[NSMenu alloc] initWithTitle: appName];
-  [menu addItem: [[NSMenuItem alloc] initWithTitle: @"About sketch..." action: NULL keyEquivalent: @""]];
-  NSMenuItem *appMenuItem = [[NSMenuItem alloc] initWithTitle: appName action: NULL keyEquivalent: @""];
-  [appMenuItem setSubmenu: appMenu];  
-
-  [[NSApp menu] addItem: appMenuItem];
-
-  [NSApp performSelector:NSSelectorFromString(@"setAppleMenu:") withObject:appMenu];
-}
-
 - (void)applicationDidFinishLaunching: (NSNotification *)notification {
-  // the event loop starts here.
-  // first initialize the window.
-  NSLog(@"Application will finish launching.");
-
-  [self createMenu: @"sketch"];
-
-  NSLog(@"Menu created.");
-
   NSRect frame = [[NSScreen mainScreen] frame];
   uint32_t x = (frame.size.width - context->width) / 2;
   uint32_t y = (frame.size.height - context->height) / 2;
   NSRect windowRect = NSMakeRect(x, y, context->width, context->height);
   window = [[[NSWindow alloc] initWithContentRect: windowRect styleMask: (NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask) backing: NSBackingStoreBuffered defer: YES] retain];
   [window setDelegate: self];
-  NSLog(@"Window created.");
   
   sketchView = [[SketchView alloc] initWithFrame: windowRect sketchContext: context];
   [window setContentView: sketchView];
-  NSLog(@"Sketch view initiated.");
 
   [window makeKeyAndOrderFront:self];
-  NSLog(@"Window made key and ordered front.");
 }
 
 - (void)windowWillClose: (NSNotification *)notification {
